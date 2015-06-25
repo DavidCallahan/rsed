8c04eb703dd3a9466336a1832612c5d8a9425b00
David Callahan <dcallahan@fb.com>
2015-06-24 23:11:18 -0700

    Initial Commit

---
+++ rsed/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  rsed
+//
+//  Created by David Callahan on 6/24/15.
+//  Copyright (c) 2015 David Callahan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
