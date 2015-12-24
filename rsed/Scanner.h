3ff36006a8472a2b4f7b11c4ee91580c121b9f51
David Callahan <dcallahan@fb.com>
2015-12-24 10:49:43 -0800

    change statement list reduction order for smaller stacks

--- rsed/Scanner.h
+++ rsed/Scanner.h
@@ -17,3 +17,20 @@ public:
   
   std::string * multilineString();
 };
+
+class StmtList {
+  Statement * head, *tail;
+public:
+  StmtList & append(Statement * s) {
+    if(head) { tail->setNext(s); }
+    else     head = s;
+    tail = s;
+    return *this;
+  }
+  Statement * getHead() const { return head; }
+  static StmtList emptyList() {
+    StmtList temp;
+    temp.head = temp.tail = nullptr;
+    return temp;
+  }
+} ;
